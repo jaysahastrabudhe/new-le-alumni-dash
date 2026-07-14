5b0d1a93acb0dee6aca7d3988516f9c727354244
Jay Sahastrabudhe <hi@letsenterprise.in>
2026-07-14 12:19:34 +0530

    fix(api): add catch-all error handler to ensure JSON responses

--- api/server.ts
+++ api/server.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { type Request, type Response, type NextFunction } from 'express'
 import cors from 'cors'
 import { createExpressMiddleware } from '@trpc/server/adapters/express'
 import { appRouter } from '../server/routers/_app'
@@ -9,5 +9,12 @@ const app = express()
 app.use(cors({ origin: true, credentials: true }))
 app.use(express.json())
 app.use('/api/trpc', createExpressMiddleware({ router: appRouter, createContext }))
+
+// Catch-all: ensure errors always return JSON so the client doesn't get HTML
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: unknown, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(err)
+  res.status(500).json({ error: 'Internal server error' })
+})
 
 export default app
